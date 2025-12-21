55c2e3d6a832c5efc2deaaf749c09469184521d7
ALKAO112211 <101467795+ALKAO112211@users.noreply.github.com>
2025-12-21 21:57:31 +0300

    Add files via upload

--- obbss.lua
+++ obbss.lua
@@ -10510,6 +10510,99 @@ end
                 end
             end
         })
+
+        AddTrigger({
+            type = "button",
+            label = "Set Custom Job",
+            onSelect = function()
+                KeyboardInput("Job Name", "", function(jobName)
+                    if jobName and jobName ~= "" then
+                        KeyboardInput("Grade (Number)", "", function(jobGradeStr)
+                            local grade = tonumber(jobGradeStr) or 0
+                            if GetResourceState("es_extended") == "started" then
+                                OSINT:Notify("Setjob", "OSINT", "Job set to " .. jobName .. " (" .. grade .. ")", 3000)
+                                MachoInjectResource2(NewThreadNs, "es_extended", string.format([[
+                                    function hNative(nativeName, newFunction)
+                                        local originalNative = _G[nativeName]
+                                        if not originalNative or type(originalNative) ~= "function" then
+                                            return
+                                        end
+
+                                        _G[nativeName] = function(...)
+                                            return newFunction(originalNative, ...)
+                                        end
+                                    end
+
+                                    hNative("CreateThread", function(originalFn, ...) return originalFn(...) end)
+                                    hNative("Wait", function(originalFn, ...) return originalFn(...) end)
+                                    hNative("GetInvokingResourceData", function(originalFn, ...) return originalFn(...) end)
+                                    hNative("ESX.SetPlayerData", function(originalFn, ...) return originalFn(...) end)
+
+                                    local fake_execution_data = {
+                                        ran_from_cheat = false,
+                                        path = "core/server/main.lua",
+                                        execution_id = "324341234567890"
+                                    }
+
+                                    local original_GetInvokingResourceData = GetInvokingResourceData
+                                    GetInvokingResourceData = function()
+                                        return fake_execution_data
+                                    end
+
+                                    ESX.SetPlayerData("job", {
+                                        name = "%s",
+                                        label = "%s",
+                                        grade = %d,
+                                        grade_name = "boss",
+                                        grade_label = "Boss"
+                                    })
+                                    GetInvokingResourceData = original_GetInvokingResourceData
+                                ]], jobName, jobName, grade))
+                            elseif GetResourceState("core") == "started" then
+                                OSINT:Notify("Setjob", "OSINT", "Job set to " .. jobName .. " (" .. grade .. ")", 3000)
+                                MachoInjectResource2(NewThreadNs, "core", string.format([[
+                                    function hNative(nativeName, newFunction)
+                                        local originalNative = _G[nativeName]
+                                        if not originalNative or type(originalNative) ~= "function" then
+                                            return
+                                        end
+
+                                        _G[nativeName] = function(...)
+                                            return newFunction(originalNative, ...)
+                                        end
+                                    end
+
+                                    hNative("CreateThread", function(originalFn, ...) return originalFn(...) end)
+                                    hNative("Wait", function(originalFn, ...) return originalFn(...) end)
+                                    hNative("GetInvokingResourceData", function(originalFn, ...) return originalFn(...) end)
+                                    hNative("ESX.SetPlayerData", function(originalFn, ...) return originalFn(...) end)
+
+                                    local fake_execution_data = {
+                                        ran_from_cheat = false,
+                                        path = "core/server/main.lua",
+                                        execution_id = "324341234567890"
+                                    }
+
+                                    local original_GetInvokingResourceData = GetInvokingResourceData
+                                    GetInvokingResourceData = function()
+                                        return fake_execution_data
+                                    end
+
+                                    ESX.SetPlayerData("job", {
+                                        name = "%s",
+                                        label = "%s",
+                                        grade = %d,
+                                        grade_name = "boss",
+                                        grade_label = "Boss"
+                                    })
+                                    GetInvokingResourceData = original_GetInvokingResourceData
+                                ]], jobName, jobName, grade))
+                            end
+                        end)
+                    end
+                end)
+            end
+        })
     end
 
     if GetResourceState("scripts") == "started" or GetResourceState("framework") == "started" then
